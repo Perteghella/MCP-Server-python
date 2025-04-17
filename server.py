4af3950bd036b443fe35cbdf3f2b9d8faf1ddccc
Giovanni Perteghella <giovanni@perteghella.org>
2025-04-17 09:22:28 +0200

    add FastAPI as AI tool

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 # server.py
+from fastapi import FastAPI
 from mcp.server.fastmcp import FastMCP
 
 # Create an MCP server
@@ -10,6 +11,7 @@ mcp = FastMCP("Demo")
 def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
+
 
 # Subtract an addition tool
 @mcp.tool()
@@ -24,8 +26,21 @@ def get_greeting(name: str) -> str:
     """Get a personalized greeting"""
     return f"Hello, {name}!"
 
+# FastAPI app
+
+app = FastAPI()
+
+@app.get("/todo/{item_id}")
+async def get_todo(item_id: int):
+    return {"id": item_id, "task": f"Task {item_id}"}
+
+@mcp.tool()
+async def get_todo_tool(item_id: int):
+    return await get_todo(item_id)
 
 # Run the server
 if __name__ == "__main__":
-    print ("Starting FastMCP server...")
+    print("Starting FastMCP server...")
     mcp.run(transport="sse")
+
+
